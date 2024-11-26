979b5a3a5e40c41ec276daae4f969532239464c7
Kiana <3092179918@qq.com>
2024-11-26 18:33:41 +0800

    🛠️删除部分冗余代码💡

--- utils/HttpUtils.go
+++ utils/HttpUtils.go
@@ -3,52 +3,13 @@ package utils
 import (
 	"BronyaBot/global"
 	"encoding/json"
-	"errors"
 	"fmt"
-	"github.com/go-resty/resty/v2"
 	"io"
 	"net/http"
 	"strings"
-	"time"
 )
 
-type HttpResponse struct {
-	Response *resty.Response
-	Error    error
-}
-
-type HttpUtils struct {
-	Client *resty.Client
-}
-
 var client = &http.Client{}
-
-func (c *HttpUtils) AsyncPostWithTimeout(
-	url string,
-	headers map[string]string,
-	params interface{},
-	timeout time.Duration,
-) (*HttpResponse, error) {
-	resultChan := make(chan HttpResponse, 1)
-	r := c.Client.R()
-	go func() {
-		if headers != nil {
-			r.SetHeaders(headers)
-		}
-		if params != nil {
-			r.SetBody(params)
-		}
-		r.SetCookie(&http.Cookie{})
-		resp, err := r.Post(url)
-		resultChan <- HttpResponse{Response: resp, Error: err}
-	}()
-	select {
-	case result := <-resultChan:
-		return &result, nil
-	case <-time.After(timeout):
-		return nil, errors.New("request timed out")
-	}
-}
 
 func SendRequest(method, url string, payload map[string]string, header http.Header) ([]byte, error) {
 	marshal, err := json.Marshal(payload)
